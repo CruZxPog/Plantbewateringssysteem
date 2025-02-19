de2f46debdbd242e8d400bd4a99c8501a8a320d4
CruZxPog <andredacruzribeiro13@gmail.com>
2025-02-19 12:34:17 +0100

    made things

--- Plantbewateringssysteem/src/config.h
+++ Plantbewateringssysteem/src/config.h
@@ -1,3 +1,25 @@
+// TODO: Wachttijd tussen 2 opeenvolgende inlezingen van sensoren
+const int READ_TIMER = 20000;
+
+
+// TODO: Temperatuur schakelwaarden
+const int TEMP_LOW = 5;
+const int TEMP_HIGH = 25;
+
+// TODO: Categoriën vochtigheid
+const String DROOG = "DROOG";
+const String VOCHTIG = "VOCHTIG";
+const String NAT = "NAT";
+
+// TODO: Statussen water geven (geen water, wél water)
+const byte PUMP_ON = HIGH;
+const byte PUMP_OFF = LOW;
+
+
+// TODO: Duurtijden water geven (in milliseconden)
+const int PUMP_TIMER_SHORT = 1000;
+const int PUMP_TIMER_LONG = 2000;
+
 /*
   Opgelet, we definiëren de intervallen als gesloten: [min, max]
 */
@@ -8,28 +30,28 @@
 
 // Minimum- en maximumwaarde voor de resistieve vochtigheidssensor om de interpretatie "DROOG" te krijgen
 const int RESISTIEVE_SENSOR_DROOG_INTERVAL_MIN = 0;
-const int RESISTIEVE_SENSOR_DROOG_INTERVAL_MAX = 1535;
+const int RESISTIEVE_SENSOR_DROOG_INTERVAL_MAX = 1792;
 
 // Minimum- en maximumwaarde voor de resistieve vochtigheidssensor om de interpretatie "VOCHTIG" te krijgen
 const int RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MIN = RESISTIEVE_SENSOR_DROOG_INTERVAL_MAX + 1;
-const int RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MAX = 1800;
+const int RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MAX = 2219;
 
 // Minimum- en maximumwaarde voor de resistieve vochtigheidssensor om de interpretatie "NAT" te krijgen
 const int RESISTIEVE_SENSOR_NAT_INTERVAL_MIN = RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MAX + 1;
-const int RESISTIEVE_SENSOR_NAT_INTERVAL_MAX = 2581;
+const int RESISTIEVE_SENSOR_NAT_INTERVAL_MAX = 2538;
 
 /*
     CAPACITIEVE_SENSOR
 */
 
 // Minimum- en maximumwaarde voor de capacitieve vochtigheidssensor om de interpretatie "DROOG" te krijgen
-const int CAPACITIEVE_SENSOR_DROOG_INTERVAL_MIN = 2881;
-const int CAPACITIEVE_SENSOR_DROOG_INTERVAL_MAX = 3003;
+const int CAPACITIEVE_SENSOR_DROOG_INTERVAL_MIN = 2514;
+const int CAPACITIEVE_SENSOR_DROOG_INTERVAL_MAX = 2849;
 
 // Minimum- en maximumwaarde voor de capacitieve vochtigheidssensor om de interpretatie "VOCHTIG" te krijgen
-const int CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MIN = 2467;
+const int CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MIN = 1853;
 const int CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MAX = CAPACITIEVE_SENSOR_DROOG_INTERVAL_MIN - 1;
 
 // Minimum- en maximumwaarde voor de capacitieve vochtigheidssensor om de interpretatie "NAT" te krijgen
-const int CAPACITIEVE_SENSOR_NAT_INTERVAL_MIN = 1666;
+const int CAPACITIEVE_SENSOR_NAT_INTERVAL_MIN = 1441;
 const int CAPACITIEVE_SENSOR_NAT_INTERVAL_MAX = CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MIN - 1;
--- Plantbewateringssysteem/src/main.cpp
+++ Plantbewateringssysteem/src/main.cpp
@@ -1,79 +1,174 @@
-#include <Arduino.h>
-//libs for temp sens 
-#include <OneWire.h>
-#include <DallasTemperature.h>
-//lib for servo
-#include <ESP32Servo.h>
-//lib for built in rgb led
-#include <FastLED.h>
+#include "Arduino.h"
+#include "OneWire.h"
+#include "DallasTemperature.h"
+#include "config.h"
 
-#define SEN0114_PIN A4
-#define SEN0193_PIN A3
-
+// DONE: Definieer juiste pinnummers voor sensoren
+#define OVERRIDE_BUTTON_PIN D7
+#define BVHR_PIN A4
+#define BVHC_PIN A3
 #define TEMPSENS_PIN D3
+#define PUMP_PIN D5
+
 OneWire oneWire(TEMPSENS_PIN);
 DallasTemperature sensors(&oneWire);
 
-#define SERVO_PIN D5
-Servo myservo;
-const int ServoOpenVal = 300;
-const int ServoCloseVal = 10;
-void OpenServo(){
-  myservo.write(ServoOpenVal);
-  
+// DONE: Variabelen om wachttijd tussen inlezen sensoren te kunnen regelen
+unsigned long lastReadTime = 0;
+
+// DONE: Variabelen om duurtijd van water geven te kunnen regelen
+unsigned long startWateringTime = 0;
+int wateringDuration = 0;
+
+// DONE: Variabele om status van de waterpomp aan te geven, dit is nodig om te kunnen controlleren of de waterpomp gestopt moet worden
+bool pumpState = false;
+
+/**
+ * Bepaal de temperatuur, op basis van de gekozen temperatuursensor.
+ * Voor een digitale sensor zal dit anders zijn dan voor een analoge.
+ * Geeft de temperatuur in °C terug.
+ */
+int leesTemperatuur() {
+  // DONE: Implementeer zodat de temperatuur op de juiste manier wordt ingelezen
+  sensors.requestTemperatures();
+  return sensors.getTempCByIndex(0);
 }
-void CloseServo(){
-  myservo.write(ServoCloseVal);
+
+/**
+ * Bepaal de categorie van de capacitieve bodemvochtigheidssensor voor de gemeten sensorwaarde.
+ * We gebruiken hierbij de configuratie uit onze calibratie.  Per categorie checken we of de waarde
+ * tussen de MIN en de MAX valt.
+ * Opgelet!!  Gebruik enkel de categoriën uit je configuratiebestand!
+ */
+String berekenCategorieCapactieveBVH(int sensorwaarde) {
+   // DONE: Implementeer zodat de categorie voor de capacitieve BVH sensor wordt berekend.
+  if (sensorwaarde >= CAPACITIEVE_SENSOR_DROOG_INTERVAL_MIN && sensorwaarde <= CAPACITIEVE_SENSOR_DROOG_INTERVAL_MAX) {
+      return "DROOG";
+  } else if (sensorwaarde >= CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MIN && sensorwaarde <= CAPACITIEVE_SENSOR_VOCHTIG_INTERVAL_MAX) {
+      return "VOCHTIG";
+  } else if (sensorwaarde >= CAPACITIEVE_SENSOR_NAT_INTERVAL_MIN && sensorwaarde <= CAPACITIEVE_SENSOR_NAT_INTERVAL_MAX) {
+      return "NAT";
+  } else {
+      return "ONBEKEND";
+  }
+   
 }
 
-#define NUM_LEDS 1     //Number of RGB LED beads
-#define DATA_PIN D8    //The pin for controlling RGB LED
-#define LED_TYPE NEOPIXEL    //RGB LED strip type
-CRGB leds[NUM_LEDS];    //Instantiate RGB LED
-
-
-int calculateMoisturePercentage(int sensorValue, int sensorMinValue, int sensorMaxValue) {
-    return map(sensorValue, sensorMinValue, sensorMaxValue, 0, 100);
+/**
+ * Bepaal de categorie van de resistieve bodemvochtigheidssensor voor de gemeten sensorwaarde.
+ * We gebruiken hierbij de configuratie uit onze calibratie.  Per categorie checken we of de waarde
+ * tussen de MIN en de MAX valt.
+ * Opgelet!!  Gebruik enkel de categoriën uit je configuratiebestand!
+ */
+String berekenCategorieResistieveBVH(int sensorwaarde) {
+  if (sensorwaarde >= RESISTIEVE_SENSOR_DROOG_INTERVAL_MIN && sensorwaarde <= RESISTIEVE_SENSOR_DROOG_INTERVAL_MAX) {
+      return "DROOG";
+  } else if (sensorwaarde >= RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MIN && sensorwaarde <= RESISTIEVE_SENSOR_VOCHTIG_INTERVAL_MAX) {
+      return "VOCHTIG";
+  } else if (sensorwaarde >= RESISTIEVE_SENSOR_NAT_INTERVAL_MIN && sensorwaarde <= RESISTIEVE_SENSOR_NAT_INTERVAL_MAX) {
+      return "NAT";
+  } else {
+      return "ONBEKEND";
+  }
 }
 
-void outputSerialSoilValues(String soilSensorType, int rawValue, int percentage) {
-  Serial.print(soilSensorType + "soil sensor value: ");
-  Serial.print(rawValue);
-  Serial.print(" - Soil moisture percentage: ");
-  Serial.print(percentage);
-  Serial.println("%");
+/**
+ * Bereken de samengestelde categorie voor beide bodemvochtigheidssensoren.
+ * Mogelijke strategiën: droogste wint altijd / één wint altijd / geen mogelijke categorie bij verschil
+ * Opgelet!!  Gebruik enkel de categoriën uit je configuratiebestand!
+ */
+String berekenSamengesteldeCategorie(String categorieResistieveBVH, String categorieCapacitieveBVH) {
+  if (categorieResistieveBVH == "DROOG" || categorieCapacitieveBVH == "DROOG") {
+    return "DROOG";
+  } else if (categorieResistieveBVH == "VOCHTIG" || categorieCapacitieveBVH == "VOCHTIG") {
+      return "VOCHTIG";
+  } else if (categorieResistieveBVH == "NAT" || categorieCapacitieveBVH == "NAT") {
+      return "NAT";
+  } else {
+      return "ONBEKEND";
+}
+}
+
+/**
+ * Zet de waterpomp aan voor een bepaalde tijd.   
+ * Opgelet!!  Deze functie mag GEEN DELAY bevatten.  De duurtijd zal dus via een variabele moeten bijgehouden worden.
+ *            Het hoofdprogramma moet telkens controlleren of de duurtijd reeds verstreken is, via millis().
+ *            Gebruik een status om aan te geven dat de waterpomp aan het water geven is.
+ */
+void zetWaterpompAan(int duurtijd) {
+  // DONE: Implementeer code om de pomp aan te zetten
+  pumpState = true;
+  digitalWrite(PUMP_PIN, PUMP_ON);
+  // DONE: Initialiseer de variabelen om de starttijd en duurtijd van het water geven te regelen
+  startWateringTime = millis();
+  wateringDuration = duurtijd;
+ 
+}
+
+/**
+ * Zet de waterpomp uit. 
+ * Opgelet!! Aangezien de zetWaterpompAan() functie geen delay bevat, zullen de variabelen die daar gebruikt worden
+ *           opnieuw geïnitialiseerd moeten worden bij het uitzetten van de pomp.
+ */
+void zetWaterpompUit() {
+  // DONE: Implementeer code om de pomp uit te zetten
+  pumpState = false;
+  digitalWrite(PUMP_PIN, PUMP_OFF);
+  // DONE: Initialiseer de variabelen om de starrtijd en duurtijd van het water geven te regelen
+  startWateringTime = 0;
+  wateringDuration = 0;
+
+}
+
+/**
+ * Deze functie bevat alle code voor het uitlezen van de sensoren en om de waterpomp indien nodig aan te zetten.
+ * Het uitzetten van de waterpomp gebeurt niet hier maar in de loop() functie na controle of er voldoende tijd verstreken is.
+ */
+void leesSensorenEnGeefWaterIndienNodig() {
+  // DONE: Implementeer inlezen met correcte pinnen
+  int capacitieve_bvh_waarde = analogRead(BVHC_PIN);
+  int resistieve_bvh_waarde = analogRead(BVHR_PIN);
+  int temperatuur = leesTemperatuur();
+
+  // Bepaal individuele categoriën en samengestelde categorie
+  String categorieCapacitieveBVH = berekenCategorieCapactieveBVH(capacitieve_bvh_waarde);
+  String categorieResistieveBVH = berekenCategorieResistieveBVH(resistieve_bvh_waarde);
+  String categorie = berekenSamengesteldeCategorie(categorieCapacitieveBVH, categorieResistieveBVH);
+
+  // DONE: Beslis over water geven en pas de controles toe uit de flowchart.  
+  // !! Gebruik enkel de constanten uit de configuratie om met een categorie te vergelijken!
+  // !! Gebruik enkel de constanten uit de configuratie om de duurtijd van het water geven mee te geven
+  // !! Gebruik verder enkel de functies zetWaterpompAan() en zetWaterpompUit() om de waterpomp aan/uit te zetten
+  if (temperatuur > TEMP_HIGH && categorie == DROOG){
+    zetWaterpompAan(PUMP_TIMER_LONG);
+  } else if (temperatuur >= TEMP_LOW && temperatuur <= TEMP_HIGH && categorie == DROOG){
+    zetWaterpompAan(PUMP_TIMER_SHORT);
+  }
 }
 
 void setup() {
+  // DONE: Implementeer de nodig code voor lezen sensoren (indien nodig)
   Serial.begin(9600);
   sensors.begin();
-  myservo.attach(SERVO_PIN);
-   FastLED.addLeds<LED_TYPE, DATA_PIN>(leds, NUM_LEDS); 
-   
-}  
+  pinMode(OVERRIDE_BUTTON_PIN, INPUT);
+  pinMode(BVHR_PIN,INPUT);
+  pinMode(BVHC_PIN,INPUT);
+  pinMode(PUMP_PIN,OUTPUT);
+  digitalWrite(PUMP_PIN,PUMP_OFF);
+}
 
 void loop() {
-  int resSensorValue = analogRead(SEN0114_PIN);
-  int resMoisturePercentage = calculateMoisturePercentage(resSensorValue, RESISTANCE_MIN, RESISTANCE_MAX); 
-
-  int capSensorValue = analogRead(SEN0193_PIN);
-  int capMoisturePercentage = calculateMoisturePercentage(capSensorValue, CAPACITANCE_MIN, CAPACITANCE_MAX);
+  // We hebben huidige millis nodig om de verschillende processen te controleren (water geven / stoppen)
+  unsigned long huidigeMillis = millis();
   
-  sensors.requestTemperatures();
-  int temperaturVal = sensors.getTempCByIndex(0);
-
-  outputSerialSoilValues("Resistance type ", resSensorValue, resMoisturePercentage);
-  outputSerialSoilValues("Capacitance type ", capSensorValue, capMoisturePercentage);
-  Serial.print("Temperature: ");
-  Serial.print(temperaturVal);
-  Serial.println("--------");
-  if(resMoisturePercentage < 50 || capMoisturePercentage < 50){
-    OpenServo;
-    leds[0] = CRGB::Red;
-  } else {
-    CloseServo;
-    leds[0] = CRGB::Green;
+  // DONE: Controleer of de waterpomp uitgezet moet worden en roep functie zetWaterpompUit() aan indien nodig
+  if (pumpState && (huidigeMillis - startWateringTime >= wateringDuration)) {
+      zetWaterpompUit();
   }
-  FastLED.show();
-  delay(500);
+  
+  // DONE: Controleer of sensoren ingelezen moeten worden en roep functie leesSensorenEnGeefWaterIndienNodig() aan indien nodig
+  if (huidigeMillis - lastReadTime >= READ_TIMER) {
+      leesSensorenEnGeefWaterIndienNodig();
+      lastReadTime = huidigeMillis;
+  }
 }
